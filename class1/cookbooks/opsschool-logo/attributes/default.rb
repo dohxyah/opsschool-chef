35e645af3c77fe74489df49214e4762a13c30a84
DoH <188313@gmail.com>
2018-02-14 10:46:37 +0200

    Update default.rb

---
+++ class1/cookbooks/opsschool-logo/attributes/default.rb
@@ -0,0 +1,4 @@
+default['site']['port'] = '80'
+default['site']['user'] = "www-data"
+default['site']['path'] = "/opt/opsschool"
+default['logo']['url'] = "https://static.wixstatic.com/media/ba2a09_b5ebfce1a107401c81962d1b4eb67b4a~mv2.png/v1/fill/w_214,h_108,al_c,lg_1/ba2a09_b5ebfce1a107401c81962d1b4eb67b4a~mv2.png"
